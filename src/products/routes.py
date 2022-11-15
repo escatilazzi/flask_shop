815fb215eb3b05cc22da0cfd3097b8499a244bab
unknown <escatilazzi@gmail.com>
2022-11-14 23:07:16 -0300

    initial commit

--- src/products/routes.py
+++ src/products/routes.py
@@ -14,22 +14,18 @@ product_bp = Blueprint("product",__name__)
 def product_detail(product_id):
 	product = Product.query.filter_by(prod_id=product_id).first()
 	category = Category.query.filter_by(cat_id=product.prod_category_id).first()
-	return render_template("products/product_detail.html", product=product, name=product.prod_name, image=product.prod_file, price=product.prod_price, quantity=product.prod_quantity, category=category, cat_name=category.cat_name)
+	comments = Comment.query.filter_by(com_product_id=product_id).all()
+	if request.method == "POST":
+		user_id=current_user.id
+		comment = request.form.get('comment')
+		new_comment = Comment(com_comment=comment,com_user_id=user_id, com_product_id=product_id )
+		db.session.add(new_comment)
+		db.session.commit()
+		flash("Gracias por dejar tu comentario")
+	return render_template("products/product_detail.html", product=product, name=product.prod_name, image=product.prod_file, price=product.prod_price, quantity=product.prod_quantity, category=category, cat_name=category.cat_name, comments=comments)
 
 @product_bp.route("/category/<int:cat_id>", methods=["GET","POST"])
 def showCategory(cat_id):
 	prod_cat = db.session.query(Product.prod_id, Product.prod_name, Product.prod_file, Product.prod_desc, Product.prod_price, Category.cat_name).join(Category).filter(Product.prod_category_id == cat_id).all()
 	category = db.session.query(Category.cat_name).filter_by(cat_id=cat_id).first()
 	return render_template("products/category.html", prod_cat=prod_cat, category=category.cat_name)
-
-@product_bp.route("/product/<int:product_id>/<int:usr_id>/comment", methods=["GET","POST"])
-@login_required
-def comment(product_id):
-	product_id = request.form.get('product_id')
-	form = AddComment()
-	if form.validate_on_submit():
-		user_id=current_user.id
-		new_comment = Comment(com_comment=form.comment.data,com_user_id=user_id,com_product_id=product_id )
-		db.session.add(new_comment)
-		flash("Gracias por dejar tu comentario")
-		return redirect(request.referrer)
